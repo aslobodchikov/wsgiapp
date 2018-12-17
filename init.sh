5501e240602c38552e2c414c0cd98bd4dc1e46f2
aslobodchikov <m1djaks@mail.ru>
2018-12-18 00:43:13 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,2 @@
-sudo ln -sf /home/box/web/nginx.conf  /nginx/sites-enabled/default
-sudo /init.d/nginx restart
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
