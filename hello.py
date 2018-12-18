9cef8aa783187635881488b02a9026ed8d07a433
aslobodchikov <m1djaks@mail.ru>
2018-12-18 11:54:41 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,4 @@
-
-    def app(environ, start_response):
+def app(environ, start_response):
     status = '200 OK' 
     headers = [ 
         ('Content-Type', 'text/plain')
@@ -7,4 +6,3 @@
     body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
     start_response('200 OK', [('Content-Type', 'text/plain')])
     return  body
- 
