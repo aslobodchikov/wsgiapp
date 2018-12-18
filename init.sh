8b0a010c4856df8ad92b58ae3ea5b37e0b5aa953
aslobodchikov <m1djaks@mail.ru>
2018-12-18 14:01:28 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-gunicorn -с /etc/gunicorn.d/hello.py hello
+gunicorn с /etc/gunicorn.d/hello.py hello
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
-gunicorn -b 0.0.0.0:8080 hello
+gunicorn b 0.0.0.0:8080 hello
