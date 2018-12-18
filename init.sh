c1867e39bf5cd814b294ccfcb86fd3b1bbfdd5ca
aslobodchikov <m1djaks@mail.ru>
2018-12-18 14:30:21 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo service gunicorn restart
+sudo rm /etc/nginx/sites-enabled/default
 gunicorn -b 0.0.0.0:8080 hello:app
